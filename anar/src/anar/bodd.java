3f64415c05948ab8f66089803704dbd205e4929a
Enkhtsetseg7 <163222120+Enkhtsetseg7@users.noreply.github.com>
2024-04-17 10:46:49 +0800

    Update bodd.java

--- anar/src/anar/bodd.java
+++ anar/src/anar/bodd.java
@@ -5,6 +5,7 @@ public class bodd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test is passed");
+		System.out.println("Enkhtsetseg bn");
 	}
 
 }
